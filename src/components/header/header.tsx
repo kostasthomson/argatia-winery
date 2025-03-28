8692ef6d80751b0315d47d05dfeef30d721fcb16
Kostas Thomson <kostasthomson12@gmail.com>
2025-03-29 01:37:06 +0200

    initial home page

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logoImg from "../../../public/images/logo.jpeg";
+
+export default function Header() {
+	return (
+		<header className="sticky top-0 bg-background mb-5 w-full h-28 grid grid-cols-3 gap-2 items-center justify-items-center shadow-lg rounded-br-3xl rounded-bl-3xl">
+			<p className="justify-self-start">burger menu</p>
+			<Image className="w-38 h-24 md:w-48 md:h-28" src={logoImg} alt="Argatia Winery Logo" />
+			{/* <p className="text-center">logo</p> */}
+		</header>
+	);
+}
